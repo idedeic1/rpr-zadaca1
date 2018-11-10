d2448acab1c79674043a0613865bc0bfba5c6322
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-11-10 20:30:57 +0100

    Kostur klase ChessPiece

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -2,6 +2,18 @@ package ba.unsa.etf.rpr;
 
 
 
-public class ChessPiece {
+abstract public class ChessPiece {
+    private String pozicija;
+    private Color boja;
+
+    public ChessPiece(String pozicija, Color boja) {
+        this.pozicija = pozicija;
+        this.boja = boja;
+    }
+
     public enum Color{BLACK, WHITE};
+    public String getPosition(){}
+    public Color getColor(){}
+    public void move(String position){}
+
 }
