1ab0186a0a6c37ed363f94c5cc4805a68472887c
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-11-10 21:02:54 +0100

    Uradeno sve u ChessPiece osim izuzetaka

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -1,19 +1,27 @@
 package ba.unsa.etf.rpr;
 
 
-
 abstract public class ChessPiece {
     private String pozicija;
     private Color boja;
 
     public ChessPiece(String pozicija, Color boja) {
+        if(pozicija.charAt(0) < 'A' || pozicija.charAt(0) > 'H' || pozicija.charAt(1) < '1' || pozicija.charAt(1) > '8') throw new IllegalChessMoveException("Tekst");
         this.pozicija = pozicija;
         this.boja = boja;
     }
 
-    public enum Color{BLACK, WHITE};
+    public enum Color{BLACK, WHITE}
     public String getPosition(){ return pozicija;}
     public Color getColor(){return boja;}
-    public void move(String position){}
+    public void move(String position){
+        String tmp = position.toUpperCase();
+        if(tmp.charAt(0) < 'A' || tmp.charAt(0) > 'H' || tmp.charAt(1) < '1' || tmp.charAt(1) > '8'){
+            this.pozicija = tmp;
+        }
+        else{
+            throw new IllegalChessMoveException("tekst");
+        }
+    }
 
 }
---
+++ src/ba/unsa/etf/rpr/IllegalChessMoveException.java
@@ -0,0 +1,5 @@
+package ba.unsa.etf.rpr;
+
+public class IllegalChessMoveException extends Exception {
+    public IllegalChessMoveException(String message) { super(message); }
+}
